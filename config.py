3b001ce3dfbf9ebed7459d8c97542fc034d4df8f
hibaelotmani31 <elotmani@et.esiea.fr>
2026-03-16 20:06:06 +0100

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-MONGO_URI = "mongodb+srv://elotmani_db_user:umcHiScYG2xY0Ypf@films-nosql.82ewqtl.mongodb.net/"
-MONGO_DB  = "entertainment"
- 
-NEO4J_URI      = "neo4j+s://1af15992.databases.neo4j.io"
-NEO4J_USER     = "1af15992"
-NEO4J_PASSWORD = "DktyHGdc-GhsRgVAHCjmInBLYVoHHLncvO1ntSB3hJY"
